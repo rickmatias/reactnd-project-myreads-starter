f1d16bd537931b754d8e7c082e5e219d23e03e7b
Ricardo Carvalho <rickmatias@msn.com>
2017-12-07 10:57:59 -0300

    refactor: refactoring BookShelf

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,80 +1,31 @@
 import React, {Component} from 'react';
-import BookSelector from './BookSelector';
+import Book from './Book';
 import PropTypes from 'prop-types';
 
 class BookShelf extends Component{
     static propTypes = {
         allBooks: PropTypes.array.isRequired,
-        shelfCategory: PropTypes.oneOf(["wantToRead", "currentlyReading", "read"]).isRequired
+        shelfCategory: PropTypes.object.isRequired
     }
 
     render(){
+        const categoryId = this.props.shelfCategory.id;
+        const categoryTitle = this.props.shelfCategory.title;
+        
         const allBooks = this.props.allBooks;
-        //const properBooks =  allBooks.filter((book) => book.)
+        const properBooks =  allBooks.filter((book) => book.shelf === categoryId);
+        
         return (
-
-
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        <div className="bookshelf">
-                            <h2 className="bookshelf-title">Currently Reading</h2>
-                            <div className="bookshelf-books">
-                                <ol className="books-grid">
-                                    <li>
-                                        <div className="book">
-                                            <div className="book-top">
-                                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                                                <BookSelector/>
-                                            </div>
-                                            <div className="book-title">To Kill a Mockingbird</div>
-                                            <div className="book-authors">Harper Lee</div>
-                                        </div>
-                                    </li>
-                                </ol>
-                            </div>
-                        </div>
-                        <div className="bookshelf">
-                        <h2 className="bookshelf-title">Want to Read</h2>
-                        <div className="bookshelf-books">
-                            <ol className="books-grid">
-                            <li>
-                                <div className="book">
-                                <div className="book-top">
-                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")' }}></div>
-                                    <BookSelector/>
-                                </div>
-                                <div className="book-title">1776</div>
-                                <div className="book-authors">David McCullough</div>
-                                </div>
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{categoryTitle}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {properBooks.map((book) => (
+                            <li key={book.id}>
+                                <Book book={book}/>
                             </li>
-                            </ol>
-                        </div>
-                        </div>
-                        <div className="bookshelf">
-                        <h2 className="bookshelf-title">Read</h2>
-                        <div className="bookshelf-books">
-                            <ol className="books-grid">
-                            <li>
-                                <div className="book">
-                                <div className="book-top">
-                                    <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: 'url("http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api")' }}></div>
-                                    <BookSelector/>
-                                </div>
-                                <div className="book-title">The Hobbit</div>
-                                <div className="book-authors">J.R.R. Tolkien</div>
-                                </div>
-                            </li>
-                            </ol>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="open-search">
-                <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                        ))}
+                    </ol>
                 </div>
             </div>
         );
