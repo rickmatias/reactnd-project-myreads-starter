ee92cf4f39debf7d1238d8c09edae486c662b360
Ricardo Carvalho <rickmatias@msn.com>
2017-12-07 13:29:28 -0300

    refactor: shelfCategories is now a static parameter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import BookShelves from './BookShelves';
-import SearchBooks from './SearchBooks';
-import * as BooksAPI from './BooksAPI';
+import React from 'react'
+import { Route } from 'react-router-dom'
+import BookShelves from './BookShelves'
+import SearchBooks from './SearchBooks'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    allBooks: [],
-    shelfCategories: [
-      {id: "wantToRead", title: "Want to Read"}, 
-      {id: "currentlyReading", title: "Currently Reading"}, 
-      {id: "read", title: "Read"}
-    ]
+    allBooks: []
   }
+
+  static shelfCategories = [
+    {id: "wantToRead", title: "Want to Read"}, 
+    {id: "currentlyReading", title: "Currently Reading"}, 
+    {id: "read", title: "Read"}
+  ]
+
+  
 
   componentDidMount(){
     BooksAPI.getAll().then((allBooks) => {
-      console.log(allBooks);
-      this.setState({allBooks});
+      console.log(allBooks)
+      this.setState({allBooks})
     });
   }
 
@@ -26,7 +29,7 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
           <Route exact path='/' render={() => (
-            <BookShelves allBooks={this.state.allBooks} shelfCategories={this.state.shelfCategories}/>
+            <BookShelves allBooks={this.state.allBooks}/>
           )}/>
           <Route path='/search' render={() => (
             <SearchBooks allBooks={this.state.allBooks}/>
