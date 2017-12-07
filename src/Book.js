ce6ad4d20d01e1cb097f3503887b630f874f4e1e
Ricardo Carvalho <rickmatias@msn.com>
2017-12-07 10:53:04 -0300

    refactor: refactoring Book to dinamically show its info

--- src/Book.js
+++ src/Book.js
@@ -1,21 +1,25 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import BookSelector from './BookSelector';
 
 class Book extends Component{
     static propTypes = {
-        book: PropTypes.Object
+        book: PropTypes.object.isRequired
       }
     render(){
         const book = this.props.book;
+        
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.backgroundImage}}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
                     <BookSelector/>
                 </div>
-                <div className="book-title">${book.title}</div>
-                <div className="book-authors">${book.author}</div>
-        </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.author}</div>
+            </div>
         );
     }
 }
+
+export default Book;
