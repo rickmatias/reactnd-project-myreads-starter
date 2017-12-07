11911d75f404317c9d52c614a43e846928d5eb16
Ricardo Carvalho <rickmatias@msn.com>
2017-12-07 11:04:32 -0300

    refactor: replacing <a> by <Link>

--- src/BookShelves.js
+++ src/BookShelves.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import BookShelf from './BookShelf';
+import {Link} from 'react-router-dom';
 
 class BookShelves extends Component{
     static propTypes = {
@@ -22,7 +23,7 @@ class BookShelves extends Component{
                     </div>
                 </div>
                 <div className="open-search">
-                    <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                <Link to='/search'>Add a book</Link>
                 </div>
             </div>
         );
