9abeb8462c807d23a79c46cc4a6f2efe34d1d822
Ricardo Carvalho <rickmatias@msn.com>
2017-12-12 10:21:20 -0300

    refactor: replace <a> by <Link>

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -9,7 +9,7 @@ class SearchBooks extends Component {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <a className="close-search">Close</a>
+                    <Link to='/' className="close-search">Close</Link>
                     <div className="search-books-input-wrapper">
                         {/*
                         NOTES: The search from BooksAPI is limited to a particular set of search terms.
