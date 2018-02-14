4349770e3cb325db3781477d8e0e0cf43216badb
Ricardo Carvalho <rickmatias@msn.com>
2018-02-14 11:29:14 -0300

    refact: make BookSelector a functional component

--- src/BookSelector.js
+++ src/BookSelector.js
@@ -1,32 +1,30 @@
-import React, {Component} from 'react';
+import React from 'react';
 import BooksApp from './App';
 import PropTypes from 'prop-types';
 
 
-class BookSelector extends Component{
-  static propTypes = {
-    chosenCategory: PropTypes.string.isRequired,
-    onChangeBookShelf: PropTypes.func.isRequired
-  }
+const BookSelector = (props) => {
+  return (
+    <div className="book-shelf-changer">
+      <select defaultValue={props.chosenCategory}
+        onChange={(e) => props.onChangeBookShelf(e.target.value)}>
+        <option value="none" disabled>Move to...</option>
+        {BooksApp.shelfCategories.map((shelfCategory) => (
+          <option
+            key={shelfCategory.id}
+            value={shelfCategory.id}>
+            {shelfCategory.title}
+          </option>
+        ))}
+        <option value="none">None</option>
+      </select>
+    </div>
+  );
+}
 
-  render(){
-    return (
-      <div className="book-shelf-changer">
-        <select defaultValue={this.props.chosenCategory}
-          onChange={(e) => this.props.onChangeBookShelf(e.target.value)}>
-          <option value="none" disabled>Move to...</option>
-          {BooksApp.shelfCategories.map((shelfCategory) => (
-            <option
-              key={shelfCategory.id}
-              value={shelfCategory.id}>
-              {shelfCategory.title}
-            </option>
-          ))}
-          <option value="none">None</option>
-        </select>
-      </div>
-    );
-  }
+BookSelector.propTypes = {
+  chosenCategory: PropTypes.string.isRequired,
+  onChangeBookShelf: PropTypes.func.isRequired
 }
 
 export default BookSelector;
