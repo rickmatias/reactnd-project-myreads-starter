0dc63182362099e7fe82f6b8e91889606bd309ba
Ricardo Carvalho <rickmatias@msn.com>
2018-02-14 11:31:53 -0300

    refact: make BookShelf a functional component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,33 +1,29 @@
-import React, {Component} from 'react';
+import React from 'react';
 import BookGrid from './BookGrid';
 import PropTypes from 'prop-types';
 
-class BookShelf extends Component{
-  static propTypes = {
-    myBooks: PropTypes.array.isRequired,
-    shelfCategory: PropTypes.object.isRequired,
-    onChangeBookShelf: PropTypes.func.isRequired
-  }
-
-  render(){
-    const {id, title} = this.props.shelfCategory;
-
-    const properBooks =  this.props.myBooks.filter(
-        (book) => book.shelf === id
-    );
-
-    return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">{title}</h2>
-        <div className="bookshelf-books">
-          <BookGrid
-            books={properBooks}
-            onChangeBookShelf={this.props.onChangeBookShelf}
-          />
-        </div>
+const BookShelf = (props) => {
+  const {id, title} = props.shelfCategory;
+  const properBooks =  props.myBooks.filter(
+      (book) => book.shelf === id
+  );
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{title}</h2>
+      <div className="bookshelf-books">
+        <BookGrid
+          books={properBooks}
+          onChangeBookShelf={props.onChangeBookShelf}
+        />
       </div>
-    );
-  }
+    </div>
+  );
+}
+
+BookShelf.propTypes = {
+  myBooks: PropTypes.array.isRequired,
+  shelfCategory: PropTypes.object.isRequired,
+  onChangeBookShelf: PropTypes.func.isRequired
 }
 
 export default BookShelf;
