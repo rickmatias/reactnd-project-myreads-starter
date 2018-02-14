b14196b7da5f6ae7d2b29856e6ee2c566ac94492
Ricardo Carvalho <rickmatias@msn.com>
2018-02-14 11:16:10 -0300

    refact: make Book a functional component

--- src/Book.js
+++ src/Book.js
@@ -1,34 +1,31 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import BookSelector from './BookSelector';
 
-class Book extends Component{
-  static propTypes = {
-    book: PropTypes.object.isRequired,
-    onChangeBookShelf: PropTypes.func.isRequired
+const Book = (props) => {
+  const book = props.book;
+
+  const updateBookShelf = (shelfId) => {
+    props.onChangeBookShelf(book, shelfId);
   }
 
-  updateBookShelf(shelfId){
-    this.props.onChangeBookShelf(this.props.book, shelfId);
-  }
-
-  render(){
-    const book = this.props.book;
-
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
-            <BookSelector
-              chosenCategory={book.shelf}
-              onChangeBookShelf={(shelfId) => this.updateBookShelf(shelfId)}
-            />
-        </div>
-        <div className="book-title">{book.title}</div>
-        <div className="book-authors">{book.author}</div>
+  return(
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})`}}></div>
+          <BookSelector
+            chosenCategory={book.shelf}
+            onChangeBookShelf={(shelfId) => updateBookShelf(shelfId)}
+          />
       </div>
-    );
-  }
+      <div className="book-title">{book.title}</div>
+      <div className="book-authors">{book.author}</div>
+    </div>
+  );
 }
 
+Book.propTypes = {
+  book: PropTypes.object.isRequired,
+  onChangeBookShelf: PropTypes.func.isRequired
+}
 export default Book;
