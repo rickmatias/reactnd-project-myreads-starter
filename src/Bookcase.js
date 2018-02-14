61481f9033756179f23726a2adebf39430809d37
Ricardo Carvalho <rickmatias@msn.com>
2018-02-14 11:16:43 -0300

    refact: make Bookcase a functional component

--- src/Bookcase.js
+++ src/Bookcase.js
@@ -1,38 +1,36 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import BookShelf from './BookShelf';
 import {Link} from 'react-router-dom';
 import BooksApp from './App';
 
-class Bookcase extends Component{
-  static propTypes = {
-    myBooks: PropTypes.array.isRequired,
-    onChangeBookShelf: PropTypes.func.isRequired
-  }
-  render(){
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            {BooksApp.shelfCategories.map((category) => (
-              <BookShelf key={category.id}
-                myBooks={this.props.myBooks}
-                shelfCategory={category}
-                onChangeBookShelf={this.props.onChangeBookShelf}
-              />
-            ))}
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to='/search'>Add a book</Link>
+const Bookcase = (props) => {
+  return(
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          {BooksApp.shelfCategories.map((category) => (
+            <BookShelf key={category.id}
+              myBooks={props.myBooks}
+              shelfCategory={category}
+              onChangeBookShelf={props.onChangeBookShelf}
+            />
+          ))}
         </div>
       </div>
-    );
-  }
+      <div className="open-search">
+        <Link to='/search'>Add a book</Link>
+      </div>
+    </div>
+  );
+}
+
+Bookcase.propTypes = {
+  myBooks: PropTypes.array.isRequired,
+  onChangeBookShelf: PropTypes.func.isRequired
 }
 
 export default Bookcase;
