3760f2816e3dad08ec41f0becabeb5f98f701b6e
Ricardo Carvalho <rickmatias@msn.com>
2018-02-14 11:37:22 -0300

    refact: make SearchBookBar  a functional component

--- src/SearchBooksBar.js
+++ src/SearchBooksBar.js
@@ -1,34 +1,32 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-class SearchBooksBar extends Component{
-  static propTypes = {
-    onUpdateQuery: PropTypes.func.isRequired,
-    query: PropTypes.string.isRequired
+const SearchBooksBar = (props) => {
+  const {query} = props.query;
+
+  const updateQuery = (q) => {
+    props.onUpdateQuery(q);
   }
 
-  updateQuery = (query) => {
-    this.props.onUpdateQuery(query);
-  }
-
-  render(){
-    const {query} = this.props.query;
-
-    return(
-      <div className="search-books-bar">
-        <Link to='/' className="close-search">Close</Link>
-        <div className="search-books-input-wrapper">
-          <input
-            type="text"
-            placeholder="Search by title or author"
-            value={query}
-            onChange={(e) => this.updateQuery(e.target.value)}
-          />
-        </div>
+  return(
+    <div className="search-books-bar">
+      <Link to='/' className="close-search">Close</Link>
+      <div className="search-books-input-wrapper">
+        <input
+          type="text"
+          placeholder="Search by title or author"
+          value={query}
+          onChange={(e) => updateQuery(e.target.value)}
+        />
       </div>
-    );
- }
+    </div>
+  );
+}
+
+SearchBooksBar.propTypes = {
+  onUpdateQuery: PropTypes.func.isRequired,
+  query: PropTypes.string.isRequired
 }
 
 export default SearchBooksBar;
